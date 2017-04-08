2c383d9b5b8caefb8dc151612653015a56c27629
Cornelius Jemison <cjemison@gogoair.com>
2017-04-08 09:39:31 -0500

    simple project setup.

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "helloWorld3"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.12" % "2.4.17"
+
+        
---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,21 @@
+/**
+  * Created by cjemison on 4/8/17.
+  */
+import akka.actor._
+
+// (1) changed the constructor here
+class HelloActor(myName: String) extends Actor {
+  def receive = {
+    // (2) changed these println statements
+    case "hello" => println("hello from %s".format(myName))
+    case _       => println("'huh?', said %s".format(myName))
+  }
+}
+
+object Main extends App {
+  val system = ActorSystem("HelloSystem")
+  // (3) changed this line of code
+  val helloActor = system.actorOf(Props(new HelloActor("Fred")), name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
+}
