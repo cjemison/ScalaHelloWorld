36b4eb3320f9b6efa46495abdbf6ab35c237e69c
Cornelius Jemison <cjemison@gogoair.com>
2017-04-08 11:07:59 -0500

    added message passing.

--- build.sbt
+++ build.sbt
@@ -4,7 +4,6 @@ version := "1.0"
 
 scalaVersion := "2.12.1"
 
-
 resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
 
 libraryDependencies += "com.typesafe.akka" % "akka-actor_2.12" % "2.4.17"
--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,21 +1,54 @@
 /**
   * Created by cjemison on 4/8/17.
   */
-import akka.actor._
 
-// (1) changed the constructor here
-class HelloActor(myName: String) extends Actor {
+import akka.pattern.ask
+import akka.actor._
+import akka.util.Timeout
+
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class HelloMessage(val name: String)
+
+class GoodByeMessage(val name: String)
+
+class MessageActor extends Actor {
+  def receive = {
+    case message: HelloMessage => {
+      sender ! "Hello %s!".format(message.name)
+    }
+    case message: GoodByeMessage => {
+      sender ! "Good Bye %s!".format(message.name)
+    }
+  }
+}
+
+class HelloActor(messageActor:ActorRef) extends Actor {
   def receive = {
     // (2) changed these println statements
-    case "hello" => println("hello from %s".format(myName))
-    case _       => println("'huh?', said %s".format(myName))
+    case message: HelloMessage => {
+      implicit val timeout = Timeout(5 seconds)
+      val future = messageActor ? message
+      val result = Await.result(future, timeout.duration).asInstanceOf[String]
+      println(result)
+    }
+    case message: GoodByeMessage => {
+      messageActor ! message
+    }
   }
 }
 
 object Main extends App {
   val system = ActorSystem("HelloSystem")
-  // (3) changed this line of code
-  val helloActor = system.actorOf(Props(new HelloActor("Fred")), name = "helloactor")
-  helloActor ! "hello"
-  helloActor ! "buenos dias"
+
+
+  val messageActor = system.actorOf(Props[MessageActor], name = "messageActor")
+  val helloActor = system.actorOf(Props(new HelloActor(messageActor)), name = "helloActor")
+
+  helloActor ! new HelloMessage("Cornelius")
+ // helloActor ! new GoodByeMessage("Cornelius")
+  //helloActor ! PoisonPill
+  system.terminate()
 }
